c272081da591f35f379842417d475f04744610ae
vicks <vicks@Victor>
2025-03-26 19:21:21 +0100

    Parte tres del trabajo

--- PruebaEntornos/src/Entornos/Hola.java
+++ PruebaEntornos/src/Entornos/Hola.java
@@ -5,7 +5,7 @@ public class Hola {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Hola Peter");
+		System.out.println("Adios Peter");
 		
 	}
 }
